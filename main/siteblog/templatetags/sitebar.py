622c2f7d6f49ea554d6c1536a3c20b4774c81f72
Dmitry Ivanov <32573956+Dmit595987@users.noreply.github.com>
2023-03-02 22:52:47 +0300

    change 02-03-23

--- main/siteblog/templatetags/sitebar.py
+++ main/siteblog/templatetags/sitebar.py
@@ -3,8 +3,14 @@ from siteblog.models import Tag, Post
 
 register = template.Library()
 
+
 @register.inclusion_tag('blog/popular_posts_tpl.html')
 def get_popular_posts(cnt=5):
     posts = Post.objects.order_by('-views')[:cnt]
     return {'posts': posts}
 
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_tags():
+    tags = Tag.objects.all()
+    return {'tags': tags}
--- main/siteblog/urls.py
+++ main/siteblog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
     path('post/<str:slug>/', GetPost.as_view(), name='post'),
     path('tag/<str:slug>/', PostByTag.as_view(), name='tag'),
+    path('search/', SearchView.as_view(), name='search'),
 
 ]
